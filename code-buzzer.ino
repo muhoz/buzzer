b119c85cbe6f0f20e4d3d183f9a986904118eed7
muhoz <71699176+muhoz@users.noreply.github.com>
2021-09-30 12:57:15 +0100

    Update code-buzzer.ino

--- code-buzzer.ino
+++ code-buzzer.ino
@@ -1,19 +1,14 @@
 
+//declaration of the pin that will be used
+const int buzzer = 4; //buzzer to arduino pin 4
 
-const int buzzer = 4; //buzzer to arduino pin 9
-
-
-void setup(){
- 
-  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
-
+void setup(){ 
+  pinMode(buzzer, OUTPUT); // set the pin as an OUTPUT
 }
 
 void loop(){
- 
-  tone(buzzer, 500); // Send 1KHz sound signal...
-  delay(1000);        // ...for 1 sec
+  tone(buzzer, 500); // function to control the buzzer (tone(X,Y,Z)),where X:PIN, Y:frequency and Z:delay
+  delay(1000);        // delay for 1 sec
   noTone(buzzer);     // Stop sound...
-  delay(1000);        // ...for 1sec
-  
+  delay(1000);        // delay for 1 sec
 }
